0b32f8820672cac69dbffce0589570abe4087f30
BotShipper <Quanaptx4869@gmail.com>
2024-08-30 22:09:16 +0700

    first commit

---
+++ 02-backend/spring-boot-library/src/main/java/com/minhquan/spring_boot_library/requestmodels/ReviewRequest.java
@@ -0,0 +1,13 @@
+package com.minhquan.spring_boot_library.requestmodels;
+
+import lombok.Data;
+
+import java.util.Optional;
+
+@Data
+public class ReviewRequest {
+
+    private double rating;
+    private Long bookId;
+    private Optional<String> reviewDescription;
+}
